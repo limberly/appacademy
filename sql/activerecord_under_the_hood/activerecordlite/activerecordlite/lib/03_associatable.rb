e96aa08256f59282c969405fbf658496f404d579
limberly <limhyungue@gmail.com>
2021-03-25 20:00:38 +0900

    Working on associatable of activerecordlite

--- sql/activerecord_under_the_hood/activerecordlite/activerecordlite/lib/03_associatable.rb
+++ sql/activerecord_under_the_hood/activerecordlite/activerecordlite/lib/03_associatable.rb
@@ -11,22 +11,38 @@ class AssocOptions
 
   def model_class
     # ...
+    class_name.constantize
   end
 
   def table_name
     # ...
+    down = class_name.downcase
+    down == "human" ? "humans" : down.pluralize
+    # class_name.downcase.pluralize
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     # ...
+    defaults = {
+      foreign_key: (name.downcase + "_id").to_sym,
+      primary_key: :id,
+      class_name: name.camelcase
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
+
+    
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     # ...
+    
   end
 end
 
