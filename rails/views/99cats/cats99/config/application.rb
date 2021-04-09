777eaa74de3d756025968b7d5dcbe679a9bf5913
Lim <limhyungue@gmail.com>
2021-04-09 23:11:54 +0900

    Removed forgery protection from 99 cats

--- rails/views/99cats/cats99/config/application.rb
+++ rails/views/99cats/cats99/config/application.rb
@@ -10,7 +10,7 @@ module Cats99
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.action_controller.default_protect_from_forgery = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
